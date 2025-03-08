4782be09f29aa53a40d0df4c2bc31ba6e843e424
Gabryel-777 <gabryeldasilvalemos@gmail.com>
2025-03-07 22:26:39 -0300

    execução dos nomes das cidades do litoral sul de sp

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Cidades do litoral sul de São Paulo");
+Thread.Sleep(1000);
+Console.WriteLine("Bertioga");
+Thread.Sleep(1000);
+Console.WriteLine("Cananéia");
+Thread.Sleep(1000);
+Console.WriteLine("Cubatão");
+Thread.Sleep(1000);
+Console.WriteLine("Guarujá");
+Thread.Sleep(1000);
+Console.WriteLine("Iguape");
+Thread.Sleep(1000);
+Console.WriteLine("Ilha Comprida");
+Thread.Sleep(1000);
+Console.WriteLine("Itanhaém");
+Thread.Sleep(1000);
+Console.WriteLine("Mongaguá");
+Thread.Sleep(1000);
+Console.WriteLine("Peruíbe");
+Thread.Sleep(1000);
+Console.WriteLine("Santos");
+Thread.Sleep(1000);
+Console.WriteLine("São Vicente");
+
